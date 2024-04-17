5da71d7084e6eeecffb3e5d5e95f8dd61a59a0c8
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-04-17 17:23:14 +0530

    introduce config reader

---
+++ lms-core/src/config/reader.rs
@@ -0,0 +1,96 @@
+use crate::config::Config;
+use reqwest::Url;
+use crate::runtime::TargetRuntime;
+
+/// Reads the configuration from a file or from an HTTP URL and resolves all linked extensions to create a ConfigModule.
+pub struct ConfigReader {
+    runtime: TargetRuntime,
+}
+
+/// Response of a file read operation
+#[derive(Debug)]
+struct FileRead {
+    content: String,
+    path: String,
+}
+
+impl ConfigReader {
+    pub fn init(runtime: TargetRuntime) -> Self {
+        Self { runtime }
+    }
+
+    /// Reads the config file and returns serialized config
+    pub async fn read<T: AsRef<str>>(&self, file: T) -> anyhow::Result<Config> {
+        let file = self.read_file(file).await?;
+        let config = Config::from_json(&file.content)?;
+        Ok(config)
+    }
+    /// Reads a file from the filesystem or from an HTTP URL
+    async fn read_file<T: AsRef<str>>(&self, file: T) -> anyhow::Result<FileRead> {
+        // Is an HTTP URL
+        let content = if let Ok(url) = Url::parse(file.as_ref()) {
+            let response = self
+                .runtime
+                .http
+                .execute(
+                    reqwest::Request::new(reqwest::Method::GET, url),
+                ) // reading config should not require key
+                .await?;
+
+            String::from_utf8(response.body.to_vec())?
+        } else {
+            // Is a file path
+
+            self.runtime
+                .file
+                .read(file.as_ref())
+                .await? // reading config should not require key
+        };
+
+        Ok(FileRead {
+            content,
+            path: file.as_ref().to_string(),
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+    use super::*;
+
+    fn get_example_config() -> String {
+        let mut parent = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        parent.pop();
+
+        parent.join("examples/config.json").to_str().unwrap().to_string()
+    }
+
+    #[tokio::test]
+    async fn test_read_file() {
+        let runtime = crate::runtime::tests::init();
+        let reader = ConfigReader::init(runtime);
+
+        let example_config = get_example_config();
+
+        let file = reader.read_file(&example_config).await.unwrap();
+
+        assert_eq!(file.path, example_config);
+    }
+
+    #[tokio::test]
+    async fn test_read() {
+        let runtime = crate::runtime::tests::init();
+        let reader = ConfigReader::init(runtime);
+        let example_config = get_example_config();
+
+        let config = reader
+            .read(example_config)
+            .await
+            .unwrap();
+        assert_eq!(config.port, 19194);
+        assert_eq!(config.auth.auth_url, "http://localhost:19191/auth");
+        assert_eq!(config.auth.totp_key, "base32encodedkey");
+        assert_eq!(config.auth.aes_key, "32bytebase64encodedkey");
+    }
+}
